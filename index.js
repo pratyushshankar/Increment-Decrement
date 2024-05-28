ca943b380929f3976d3b5bfb5afdbcb745623bd0
Pratyush Shankar <114212267+pratyushshankar@users.noreply.github.com>
2024-05-28 18:09:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+const countValue = document.querySelector('#counter');
+
+function increment () {
+    //get the value from UI
+     let value = parseInt (countValue.innerText);
+     //Update the value
+     value =value +1;
+     //set the value onto UI
+     countValue.innerText =value;
+}
+
+function decrement ()  {
+
+        //get the value from UI
+        let value = parseInt (countValue.innerText);
+        //Update the value
+        value =value -1;
+        //set the value onto UI
+        countValue.innerText =value;
+   };
+   
